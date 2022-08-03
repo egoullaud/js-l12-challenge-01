ebf2c81430523ec9f24224e7c68dcf4dc726e025
elizabeth.goullaud <egoullaud@gmail.com>
2022-08-03 13:42:00 -0700

    added display image, button listener to js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-// const button = document.querySelector("button");
-// const authorSpan = document.querySelector(".author");
-// const imgDiv = document.querySelector(".image-container");
-// const img = document.querySelector(".img");
-
-// const getImage = async function(){
-//   const res = await fetch("https://picsum.photos/v2/list?limit=100");
-//   const images = await res.json();  
-//     console.log(images);
-// };
-
-// getImage();
 const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
@@ -21,13 +9,26 @@ const getImage = async function () {
   // console.log(images);
 selectRandomImage(images);
 };
+// getImage();
 
-getImage();
 
 const selectRandomImage = function(images){
   const randomIndex = Math.floor(Math.random()*images.length);
   // console.log(randomIndex);
-  const randomImages = images[randomIndex];
-  console.log(randomImages);
+  const randomImage = images[randomIndex];
+  // console.log(randomImages);
+  displayImage(randomImage);
 };
 
+const displayImage = function(randomImage){
+const author = randomImage.author;
+const imageAddress = randomImage.download_url;
+authorSpan.innerText = author;
+img.src = imageAddress;
+imgDiv.classList.remove("hide");
+
+};
+
+button.addEventListener("click", function(){
+  getImage();
+});
